b81269c9a11f9befea259dbf54cb6a965e004a68
lucas nicolas gugliuzza <lucasgugliuzza@outlook.com>
2022-10-14 18:00:37 -0300

    Initial Commit

---
+++ To-Do-List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do-List
+//
+//  Created by Lucas Gugliuzza on 14/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To-Do-List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do-List
+//
+//  Created by Lucas Gugliuzza on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
